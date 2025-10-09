230f8a62f37406585175495fd7ec783523e7dc1f
Yatharth Nagpal <122671515+Yatharthnagpal@users.noreply.github.com>
2025-10-09 20:14:38 +0530

    Time: 0 ms (100%), Space: 17.9 MB (29.77%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -7,8 +7,8 @@ class Solution:
         T=[0] * (n+1)
         T[1]=1
         T[2]=1
-        for i in range(3,n+1):
-            T[i]=T[i-2]+T[i-1]+T[i-3]
+        for i in range(n-2):
+            T[i+3]=T[i]+T[i+1]+T[i+2]
         return T[n]
             
             
