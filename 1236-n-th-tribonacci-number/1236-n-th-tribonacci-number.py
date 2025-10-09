21947565221f75876b9c5cbf273e5a146a06d519
Yatharth Nagpal <122671515+Yatharthnagpal@users.noreply.github.com>
2025-10-09 20:38:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -1,14 +1,10 @@
+from functools import lru_cache
 class Solution:
     def tribonacci(self, n: int) -> int:
         if n==0:
             return 0
-        if n==1 or n==2:
+        elif n==1 or n==2:
             return 1
-        T=[0] * (n+1)
-        T[1]=1
-        T[2]=1
-        for i in range(n-2):
-            T[i+3]=T[i]+T[i+1]+T[i+2]
-        return T[n]
+        return self.tribonacci(n-1)+self.tribonacci(n-2)+self.tribonacci(n-3)
             
             
