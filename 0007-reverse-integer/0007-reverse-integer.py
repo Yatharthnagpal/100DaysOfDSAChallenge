56ba49f9e09f9f4f96b64291fa434c9e362f95e4
Yatharth Nagpal <122671515+Yatharthnagpal@users.noreply.github.com>
2025-11-07 03:22:36 +0530

    Time: 43 ms (31.55%), Space: 17.7 MB (83.03%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,19 +1,17 @@
 class Solution:
     def reverse(self, x: int) -> int:
         ans=0
-        if x>0:
-            r=0 
+        def prevse(self,x:int):
+            r=0
             while x>0:
-              r = (x%10) + r*10
-              x = x//10
-            ans=r 
-        else:
-            y=abs(x)
-            r=0 
-            while y>0:
-              r = (y%10) + r*10
-              y = y//10
-            ans=-r
-        if ans>((2**31)-1) or ans<(-2**31):
-            ans = 0  
+                r=(x%10)+r*10
+                x=x//10
+            return r
+            
+        if x>=0:
+            ans= prevse(self,x)
+        if x<0:
+            ans= (-1)*(prevse(self,abs(x)))
+        if ans>2**31 or ans<-2**31:
+            return 0
         return ans
