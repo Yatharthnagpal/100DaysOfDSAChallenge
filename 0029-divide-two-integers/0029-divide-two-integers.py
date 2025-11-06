8903f2e522144b0642555b34ad26fcf2418084b3
Yatharth Nagpal <122671515+Yatharthnagpal@users.noreply.github.com>
2025-11-07 02:24:28 +0530

    Time: 0 ms (100%), Space: 18 MB (23.23%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def divide(self, d1: int, d2: int) -> int:
+        if d1==-2**31 and d2==-1:
+            return (2**31)-1
+        if d1<0 and d2<0:
+            return (d1)//(d2)
+        elif d1<0 or d2<0:
+            return (-(abs(d1)//abs(d2)))
+        else :
+            return d1//d2
